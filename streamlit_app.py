a28009bfce426c753c5229c6516b376da20005b6
minshanX <70848588+minshanX@users.noreply.github.com>
2025-05-08 19:17:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', 
                    )
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
